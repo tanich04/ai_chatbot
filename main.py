c40b922510817efc8a0088cb9d11d3c96377ce93
tanich04 <132200833+tanich04@users.noreply.github.com>
2025-06-27 22:14:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,18 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from bot import build_bot
 from langchain_core.messages import HumanMessage
-from fastapi.middleware.cors import CORSMiddleware
+import logging
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allow Streamlit to talk to this API
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 workflow = build_bot()
+
+logging.basicConfig(level=logging.INFO)
 
 class Query(BaseModel):
     question: str
 
 @app.post("/chat")
 def chat(req: Query):
+    logging.info(f"Incoming: {req.question}")
     result = workflow.invoke({"messages": [HumanMessage(content=req.question)]})
     return {"response": result["messages"][-1].content}
